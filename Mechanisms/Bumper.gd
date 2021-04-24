0b68e2c9ae4429556c6d57177cabd6987bee0fa1
pizzasgood <pizzasgood@gmail.com>
2021-04-24 11:51:44 -0400

    Add bumpers

---
+++ Mechanisms/Bumper.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+export var force := 750.0
+
+onready var sfx : AudioStreamPlayer2D = find_node("SFX")
+
+func _on_Bumper_body_entered(body: RigidBody2D) -> void:
+	if not body is RigidBody2D:
+		return
+	if body.is_in_group("balls"):
+		sfx.play()
+		body.apply_central_impulse(force * (body.position - position).normalized())
